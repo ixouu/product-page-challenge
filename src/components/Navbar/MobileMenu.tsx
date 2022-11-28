fdfccc11d0eb5cf6e738b1559fbded4fca4c921a
ixouu <julienchanon@gmail.com>
2022-11-28 16:18:51 +0100

    fixing nav position

--- src/components/Navbar/MobileMenu.tsx
+++ src/components/Navbar/MobileMenu.tsx
@@ -3,7 +3,7 @@ const MobileMenu = () => {
     const animationsDelays = ['150', '300', '450'];
 
 	return (
-		<nav className='animate-dropDownActive absolute top-[65px] h-[40vh] w-full left-0 scale-0 bg-blue-900 rounded-br-[200px] origin-top-left before:absolute before:content-[""] before:left-0 before:w-full before:h-[90%] before:top-0 before:rounded-br-[200px] before:bg-gradient-to-b from-zinc-900 to-blue-700 before:animate-mobileNavAfter before:origin-top-left before:scale-0'>
+		<nav className='animate-dropDownActive absolute top-[75px] h-[40vh] w-full left-0 scale-0 bg-blue-900 rounded-br-[200px] origin-top-left before:absolute before:content-[""] before:left-0 before:w-full before:h-[90%] before:top-0 before:rounded-br-[200px] before:bg-gradient-to-b from-zinc-900 to-blue-700 before:animate-mobileNavAfter before:origin-top-left before:scale-0'>
 			<ul className='flex flex-col justify-between mt-20'>
 				{menuTitles.map((title, index) => {
 					return (
